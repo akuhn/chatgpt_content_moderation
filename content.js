ed686b92dba31b02849d21d71d8d0f0565494983
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-04-23 21:10:49 +0100

    Rewrite using then.

--- content.js
+++ content.js
@@ -7,31 +7,35 @@
   window.fetch = async function(url, options) {
 
     if (url.match(/\/backend-api\/conversation\/[0-9a-f]+/)) {
-      let response = await __fetch(url, options);
-      let data = await response.json();
-      // console.log(url, data);
-      for (let each of data.moderation_results) {
-        if (each.blocked) {
-          each.blocked = false;
-          each.flagged = true;
-        }
-      }
-      response.json = async () => data;
-      return response;
+      return __fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+          for (let each of data.moderation_results) {
+            if (each.blocked) {
+              each.blocked = false;
+              each.flagged = true;
+            }
+          }
+          let response = new Response(JSON.stringify(data));
+          response.headers.set('Content-Type', 'application/json');
+          return response;
+        });
     }
 
     if (url.includes("/backend-api/moderations")) {
-      let response = await __fetch(url, options);
-      let data = await response.json();
-      // console.log(url, data);
-      if (data.blocked) {
-        console.log("Downgraded moderation from blocked to flagged message.", moderation_id);
-        data.blocked = false;
-        data.flagged = true;
-      }
-      console.log(url, data);
-      response.json = async () => data;
-      return response;
+      return __fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+          if (data.blocked) {
+            console.log("Downgraded moderation from blocked to flagged message.", moderation_id);
+            data.blocked = false;
+            data.flagged = true;
+          }
+          console.log(url, data);
+          let response = new Response(JSON.stringify(data));
+          response.headers.set('Content-Type', 'application/json');
+          return response;
+        });
     }
 
     return __fetch(url, options);
